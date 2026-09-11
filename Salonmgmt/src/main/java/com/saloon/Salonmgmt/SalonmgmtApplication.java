3650fdbdef5b906826f9ba8281c0ce4f96b2be45
Rahul120409 <virkarrahul43@gmail.com>
2026-09-11 11:45:04 +0530

    first commit

---
+++ Salonmgmt/src/main/java/com/saloon/Salonmgmt/SalonmgmtApplication.java
@@ -0,0 +1,13 @@
+package com.saloon.Salonmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonmgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonmgmtApplication.class, args);
+	}
+
+}
---
+++ Salonmgmt/src/test/java/com/saloon/Salonmgmt/SalonmgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saloon.Salonmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonmgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
